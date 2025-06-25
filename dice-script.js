1b63b5a2ef5519e48a42394abfbda39afdc5f755
majjtaj <majam.madsen@proton.me>
2025-06-25 13:12:36 +0200

    Create dice-script.js

---
+++ dice-script.js
@@ -0,0 +1,19 @@
+function rollDice() {
+  const dice = document.getElementById('dice');
+  let rollCount = 10;
+  let currentRoll = 0;
+
+  const interval = setInterval(() => {
+    const num = Math.floor(Math.random() * 6) + 1;
+    dice.src = `dice${num}.png`;
+    dice.style.transform = `rotate(${Math.random() * 360}deg)`;
+    currentRoll++;
+
+    if (currentRoll >= rollCount) {
+      clearInterval(interval);
+      const final = Math.floor(Math.random() * 6) + 1;
+      dice.src = `dice${final}.png`;
+      dice.style.transform = `rotate(0deg)`;
+    }
+  }, 100);
+}
